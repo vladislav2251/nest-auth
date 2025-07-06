1e41476fba3c1564b4627875207aec187d33e1cb
Vlad Didyk <vladdidyk1985@gmail.com>
2025-07-06 11:00:42 +0300

    chore: main.ts config

--- nestjs-server/src/main.ts
+++ nestjs-server/src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import * as cookieParser from 'cookie-parser'
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService)
+
+  app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')))
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true
+    })
+  )
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN')>,
+    credentials: true,
+    exposedHeaders: ['set-cookie']
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
