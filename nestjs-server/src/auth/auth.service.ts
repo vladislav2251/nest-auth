8bd1fc5cf7a1f41774c2ca08e60df135f26d3e43
Vlad Didyk <vladdidyk1985@gmail.com>
2025-07-07 10:28:12 +0300

    feat: redis save session

--- nestjs-server/src/auth/auth.service.ts
+++ nestjs-server/src/auth/auth.service.ts
@@ -1,19 +1,20 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { UserService } from '@/user/user.service';
 import { AuthMethod, User } from 'prisma/__generated__';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthService {
     public constructor(private readonly userService: UserService) {}
 
-    public async register(dto: RegisterDto) {
-        const isExists = await this.userService.findByEmail(dto.email)
+    public async register(req: Request, dto: RegisterDto) {
+        const isExists = await this.userService.findByEmail(dto.email);
 
         if (isExists) {
             throw new ConflictException(
-                'User with this email is exists'
-            )
+                'A user with this email already exists.'
+            );
         }
 
         const newUser = await this.userService.create(
@@ -23,16 +24,30 @@ export class AuthService {
             '',
             AuthMethod.CREDENTIALS,
             false
-        )
+        );
 
-        return this.saveSessaion(newUser)
+        return this.saveSession(req, newUser);
     }
 
     public async login() {}
 
     public async logout() {}
 
-    private async saveSessaion(user: User) {
-        console.log('Session saved. User: ', user)
+    private async saveSession(req: Request, user: User) {
+        return new Promise((resolve, reject) => {
+            req.session.userId = user.id;
+
+            req.session.save(err => {
+                if (err) {
+                    return reject(
+                        new InternalServerErrorException(
+                            'Failed to create session. Please check session configuration.'
+                        )
+                    );
+                }
+
+                resolve({ user });
+            });
+        });
     }
 }
