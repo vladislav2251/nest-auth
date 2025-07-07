6477a5be35546204806864d4f34c37e6ba406129
Vlad Didyk <vladdidyk1985@gmail.com>
2025-07-07 15:14:06 +0300

    feat: login and logout

--- nestjs-server/src/auth/auth.controller.ts
+++ nestjs-server/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +12,17 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   public async register(@Req() req: Request, @Body() dto: RegisterDto) {
     return this.authService.register(req, dto)
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  public async login(@Req() req: Request, @Body() dto: LoginDto) {
+    return this.authService.login(req, dto)
+  }
+
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  public async logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+    return this.authService.logout(req, res)
   }
 }
--- nestjs-server/src/auth/auth.service.ts
+++ nestjs-server/src/auth/auth.service.ts
@@ -1,12 +1,15 @@
-import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { UserService } from '@/user/user.service';
 import { AuthMethod, User } from 'prisma/__generated__';
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import { LoginDto } from './dto/login.dto';
+import { verify } from 'argon2';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-    public constructor(private readonly userService: UserService) {}
+    public constructor(private readonly userService: UserService, private readonly configService: ConfigService ) {}
 
     public async register(req: Request, dto: RegisterDto) {
         const isExists = await this.userService.findByEmail(dto.email);
@@ -29,9 +32,43 @@ export class AuthService {
         return this.saveSession(req, newUser);
     }
 
-    public async login() {}
+    public async login(req: Request, dto: LoginDto) {
+        const user = await this.userService.findByEmail(dto.email)
 
-    public async logout() {}
+        if (!user || !user.password) {
+            throw new NotFoundException(
+                'User not found or registration incomplete'
+            )
+        }
+
+        const isValidPassword = await verify(user.password, dto.password)
+
+        if (!isValidPassword) {
+            throw new UnauthorizedException(
+                'Invalid password. Please try again or use the password recovery option'
+            )
+        }
+
+        return this.saveSession(req, user)
+    }
+
+    public async logout(req: Request, res: Response):Promise<void> {
+        return new Promise((resolve, reject) => {
+            req.session.destroy(err => {
+                if(err) {
+                    return reject(
+                        new InternalServerErrorException(
+                            'не удаолось завершить сессии возможно возникла пробоеа на сервере или сессия уже была хавершина'
+                        )
+                    )
+                }
+                res.clearCookie(
+                    this.configService.getOrThrow<string>('SESSION_NAME')
+                )
+                resolve()
+            })
+        })
+    }
 
     private async saveSession(req: Request, user: User) {
         return new Promise((resolve, reject) => {
