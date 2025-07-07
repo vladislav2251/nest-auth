be7bc68721831aeff353b74fb9024c77a41c9099
Vlad Didyk <vladdidyk1985@gmail.com>
2025-07-07 16:18:22 +0300

    feat: find user by id

--- nestjs-server/src/user/user.controller.ts
+++ nestjs-server/src/user/user.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Authorized } from '@/auth/decorators/authorized.decorator';
+import { Authorization } from '@/auth/decorators/auth.decorator';
+import { UserRole } from 'prisma/__generated__';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Authorization()
+  @HttpCode(HttpStatus.OK)
+  @Get('profile')
+  public async findProfile(@Authorized('id') userId: string) {
+    return this.userService.findById(userId)
+  }
+
+  @Authorization(UserRole.ADMIN)
+  @HttpCode(HttpStatus.OK)
+  @Get('by-id/:id')
+  public async findById(@Param('id') id: string) {
+    return this.userService.findById(id)
+  }
 }
