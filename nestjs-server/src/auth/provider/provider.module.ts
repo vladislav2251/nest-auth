7a38190c9946069b4961365775d15a5f82d1b9f4
Vlad Didyk <vladdidyk1985@gmail.com>
2025-07-08 10:13:30 +0300

    feat: provider module

--- nestjs-server/src/auth/provider/provider.module.ts
+++ nestjs-server/src/auth/provider/provider.module.ts
@@ -1,7 +1,36 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { ProviderService } from './provider.service';
+import { ProviderOptionSymbol, TypeAsyncOptions, TypeOptions } from './services/provider.constants';
 
-@Module({
-  providers: [ProviderService],
-})
-export class ProviderModule {}
+@Module({})
+export class ProviderModule {
+  public static register(options: TypeOptions): DynamicModule {
+    return {
+      module: ProviderModule,
+      providers: [
+        {
+          useValue: options.services,
+          provide: ProviderOptionSymbol
+        },
+        ProviderService
+      ],
+      exports: [ProviderService]
+    }
+  }
+
+  public static registerAsync(options: TypeAsyncOptions): DynamicModule {
+    return {
+      module: ProviderModule,
+      imports: options.imports,
+      providers: [
+        {
+          useFactory: options.useFactory,
+          provide: ProviderOptionSymbol,
+          inject: options.inject
+        },
+        ProviderService
+      ],
+      exports: [ProviderService]
+    }
+  }
+}
