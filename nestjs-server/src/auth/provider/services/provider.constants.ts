a7ea8c81d06184fbed5e782ecd913d737d89680d
Vlad Didyk <vladdidyk1985@gmail.com>
2025-07-08 10:01:09 +0300

    feat: provider constants

---
+++ nestjs-server/src/auth/provider/services/provider.constants.ts
@@ -0,0 +1,12 @@
+import { FactoryProvider, ModuleMetadata } from "@nestjs/common"
+import { BaseOAuthService } from "./base-oauth.service"
+
+export const ProviderOptionSymbol = Symbol()
+
+export type TypeOptions = {
+    baseUrl: string
+    servces: BaseOAuthService[]
+}
+
+export type TypeAsyncOptions = Pick<ModuleMetadata, 'imports'> &
+    Pick<FactoryProvider<TypeOptions>, 'useFactory' | 'inject'>
