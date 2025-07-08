7c72bef2f97c6adb32da6c8241e4740cd217cde4
Vlad Didyk <vladdidyk1985@gmail.com>
2025-07-08 15:30:50 +0300

    feat: finish oauth

--- nestjs-server/src/auth/auth.controller.ts
+++ nestjs-server/src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { Request, Response } from 'express';
 import { LoginDto } from './dto/login.dto';
 import { Recaptcha } from '@nestlab/google-recaptcha';
+import { AuthProviderGuard } from './guards/provider.guard';
+import { ProviderService } from './provider/provider.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('auth')
 export class AuthController {
-  public constructor(private readonly authService: AuthService) {}
+  public constructor(
+    private readonly authService: AuthService,
+    private readonly providerService: ProviderService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Recaptcha()
   @Post('register')
@@ -23,8 +30,35 @@ export class AuthController {
     return this.authService.login(req, dto)
   }
 
-  @Get()
+  @Get('/oauth/callback/:provider')
+  @UseGuards(AuthProviderGuard)
+  public async callback(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+    @Query('code') code: string,
+    @Param('provider') provider: string
+  ) {
+    if(!code) {
+      throw new BadRequestException(
+        'Не был предоставлен код авторизации.'
+      )
+    }
+
+    await this.authService.extractProfileFromCode(req, provider, code)
+
+    return res.redirect(`${this.configService.getOrThrow<string>('ALLOWED_ORIGIN')}/dashboard/settings`)
+  }
+
+  @Get('/oauth/connect/:provider')
   @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthProviderGuard)
+  public async connect(@Param('provider') provider: string) {
+    const providerInstance = this.providerService.findByService(provider)
+
+    return {
+      url: providerInstance?.getAuthUrl()
+    }
+  }
 
   @Post('logout')
   @HttpCode(HttpStatus.OK)
