643a16605c00bd2572f7cda58b0a0f23b9959dfa
Vlad Didyk <vladdidyk1985@gmail.com>
2025-07-08 13:15:30 +0300

    fix: auth service

--- nestjs-server/src/auth/auth.service.ts
+++ nestjs-server/src/auth/auth.service.ts
@@ -6,10 +6,17 @@ import { Request, Response } from 'express';
 import { LoginDto } from './dto/login.dto';
 import { verify } from 'argon2';
 import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '@/prisma/prisma.service';
+import { ProviderService } from './provider/provider.service';
 
 @Injectable()
 export class AuthService {
-    public constructor(private readonly userService: UserService, private readonly configService: ConfigService ) {}
+    public constructor(
+            private readonly userService: UserService,
+            private readonly configService: ConfigService,
+            private readonly prismaService: PrismaService,
+            private readonly providerService: ProviderService
+        ) {}
 
     public async register(req: Request, dto: RegisterDto) {
         const isExists = await this.userService.findByEmail(dto.email);
@@ -51,6 +58,68 @@ export class AuthService {
 
         return this.saveSession(req, user)
     }
+
+    public async extractProfileFromCode(
+        req: Request,
+        provider: string,
+        code: string
+    ) {
+        const providerInstance = this.providerService.findByService(provider);
+
+        if (!providerInstance) {
+            throw new NotFoundException('Провайдер не найден');
+        }
+
+        const profile = await providerInstance.findUserByCode(code);
+
+        const { email, name, picture, provider: providerName, id, access_token, refresh_token, expires_at } = profile;
+
+        if (!email || !name || !providerName) {
+            throw new InternalServerErrorException('Профиль не содержит обязательных данных');
+        }
+
+        const account = await this.prismaService.account.findFirst({
+            where: {
+                id,
+                provider: providerName
+            }
+        });
+
+        let user = account?.userId
+            ? await this.userService.findById(account.userId)
+            : null;
+
+        if (!user) {
+            user = await this.userService.create(
+                email,
+                '',
+                name,
+                picture,
+                AuthMethod[providerName.toUpperCase() as keyof typeof AuthMethod],
+                true
+            );
+        }
+
+        if (!account) {
+            if (!expires_at) {
+                throw new InternalServerErrorException('Нет времени истечения access токена');
+            }
+
+            await this.prismaService.account.create({
+                data: {
+                    userId: user.id,
+                    type: 'oauth',
+                    provider: providerName,
+                    accessToken: access_token,
+                    refreshToken: refresh_token,
+                    expiresAt: expires_at
+                }
+            });
+        }
+
+        return this.saveSession(req, user);
+    }
+
 
     public async logout(req: Request, res: Response):Promise<void> {
         return new Promise((resolve, reject) => {
--- nestjs-server/src/auth/provider/services/types/user-info.types.ts
+++ nestjs-server/src/auth/provider/services/types/user-info.types.ts
@@ -2,6 +2,7 @@ export type TypeUserInfo = {
     name: string
     picture: string
     email: string
+    id:string
     access_token? : string | null
     refresh_token: string
     expires_at?: number
